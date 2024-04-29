7be48b4e357be3aafaa56b51775283a69aa88359
Abdu-Daiyaan Jattiem <AbduJ@biovac.co.za>
2024-04-29 15:08:07 +0200

    File clean up

--- app.py
+++ app.py
@@ -1,15 +1,12 @@
-from flask import Flask, flash, render_template, request, redirect, send_file, after_this_request, session
+from flask import Flask, flash, send_file, after_this_request, render_template
 from bs4 import BeautifulSoup
-from win32com.client.gencache import EnsureDispatch
 from pathlib import PurePath
-import pythoncom
-import secrets, os, threading, tabula, zipfile, pandas as pd
+from functions import *
+import secrets, zipfile
 
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = secrets.token_hex()
-UPLOAD_FOLDER = 'file_handler\\'
-lock = threading.Lock()
 password = secrets.token_hex()
 
 
@@ -17,7 +14,8 @@ password = secrets.token_hex()
 @app.route('/', methods=['GET', 'POST'])
 def index():
     html_tables = {}
-    checkbox_value = False
+    combine_tables = False
+    encrypt_workbook = False
     global index_error
 
     if 'USER_IP' not in session:
@@ -106,8 +104,14 @@ def index():
             index_error = False
 
             if selected_sheets:
+                # Check for Combine Tables checkbox
                 if 'combineTables' in request.form:
-                    checkbox_value = True 
+                    combine_tables = True 
+
+                # Check for Encrypt Workbook checkbox
+                if 'encryptWorkbook' in request.form:
+                    encrypt_workbook = True
+                print(f"* Encrypt Workbook: {encrypt_workbook}")
 
                 run_file_check_on(session.get('NEW_EXCEL_FILE_NAME', None))
                 session['NEW_EXCEL_FILE_NAME'] = 'custom_' + session.get('EXCEL_FILE_NAME')
@@ -124,8 +128,8 @@ def index():
 
                 # Writing selected sheets to excel file
                 with pd.ExcelWriter(os.path.join(f'{UPLOAD_FOLDER}{session.get("NEW_EXCEL_FILE_NAME")}'), engine='xlsxwriter') as new_excel_data:
-                    if checkbox_value:
-                        print(f"* Combine tables: {checkbox_value}")
+                    if combine_tables:
+                        print(f"* Combine tables: {combine_tables}")
                         combined_sheets = []
                         for sheet_name in selected_sheets:
                             sheet_data = excel_data.parse(sheet_name, header=1)
@@ -152,7 +156,7 @@ def index():
                             redirect(request.url)
                     else:
                         # Writing individual sheets to Excel file without concatenation
-                        print(f"* Combine tables: {checkbox_value}")
+                        print(f"* Combine tables: {combine_tables}")
                         for sheet_name in selected_sheets:
                             sheet_data = excel_data.parse(sheet_name, header=1)
 
@@ -166,18 +170,20 @@ def index():
                                 sheet_data[unnamed_columns] = sheet_data[unnamed_columns].fillna('')                       
                                 sheet_data.columns = [col if 'Unnamed' not in str(col) else '' for col in sheet_data.columns]
                                 sheet_data.to_excel(new_excel_data, sheet_name=sheet_name, index=False)
-                
-                dir_path = os.getcwd()
-                with lock:
-                    protect_this_file =  str(PurePath(dir_path, f"{UPLOAD_FOLDER}{session.get('NEW_EXCEL_FILE_NAME')}"))
-                    password_protect_excel(protect_this_file, password)
+
+                # If user wants to encrypt the Excel Workbook
+                if encrypt_workbook:
+                    dir_path = os.getcwd()
+                    with lock:
+                        protect_this_file =  str(PurePath(dir_path, f"{UPLOAD_FOLDER}{session.get('NEW_EXCEL_FILE_NAME')}"))
+                        password_protect_excel(protect_this_file, password)
 
                 excel_data.close()
                 print(f'* {len(selected_sheets)} sheets successfully generated!')
                 print('* Awaiting download request...')
                 generated_excel = os.path.join(UPLOAD_FOLDER, session.get("NEW_EXCEL_FILE_NAME"))
 
-                return render_template('index.html', html_tables=html_tables, generated_excel=generated_excel, checkbox_value=checkbox_value, index_error=index_error)
+                return render_template('index.html', html_tables=html_tables, generated_excel=generated_excel, combine_tables=combine_tables, encrypt_workbook=encrypt_workbook, index_error=index_error)
 
     return render_template('index.html', html_tables=html_tables)
 
@@ -216,128 +222,6 @@ def download_excel_file(filename):
     zip_response = send_file(zip_file_path, as_attachment=True)
     return zip_response
 
-
-def do_conversion(file):
-    """
-    Takes in pdf file, runs check if it has tables on or not. 
-    If tables exist excel file will be created and saved for further processing.
-    """
-
-    if not file.filename.endswith('.pdf'):
-        flash("File extension must be '.pdf' only.")
-        threading.Timer(1.0, run_file_check_on, args=(session.get('PDF_FILE_NAME'),))
-        threading.Timer(1.0, run_file_check_on, args=(session.get('EXCEL_FILE_NAME'),))
-        threading.Timer(1.0, run_file_check_on, args=(session.get('NEW_EXCEL_FILE_NAME'),))
-
-        return redirect(request.url)
-    
-    run_file_check_on(session.get('PDF_FILE_NAME', None))
-
-    session['PDF_FILE_NAME'] = file.filename
-    session['UPLOADS'].append(session['PDF_FILE_NAME'])
-    file.save(os.path.join(UPLOAD_FOLDER, session['PDF_FILE_NAME']))
-    
-    print(f"* File '{session['PDF_FILE_NAME']}' has been saved!")
-    print('* Converting...')
-
-    # Excel conversion
-    # Getting Files
-    extracted_file_path = os.path.join(UPLOAD_FOLDER, session['PDF_FILE_NAME'])
-    pdf_file = extracted_file_path
-    excel_file = extracted_file_path.replace('.pdf', '.xlsx')
-
-    # Reading tables
-    tables = tabula.read_pdf(pdf_file, pages='all')
-
-    # Writing tables to excel file
-    if tables:        
-        print('* Tables exist: True')
-        excel_writer = pd.ExcelWriter(excel_file, engine='xlsxwriter')
-        for i, table in enumerate(tables):
-            table.to_excel(excel_writer, sheet_name=f'Sheet_{i+1}', index=False)
-
-        excel_writer.close()
-
-        run_file_check_on(session.get('EXCEL_FILE_NAME', None))
-        
-        session['EXCEL_FILE_NAME'] = excel_file.replace(UPLOAD_FOLDER, '')
-        session['UPLOADS'].append(session['EXCEL_FILE_NAME'])
-        print('* Conversion Successful!')
-        print(f"* File '{session["EXCEL_FILE_NAME"]}' contents displayed successfully!")
-        flash('Conversion completed!')
-        flash("Use the check boxes to select the tables that you want to generate for your Excel file. Then click 'Generate Excel' at the bottom of the page ")
-    else:
-        print('* Tables exist: False')
-        run_file_check_on(session.get('EXCEL_FILE_NAME', None))
-
-    redirect(request.url)
-
-
-def remove_files(pdf_file_name, excel_file_name, new_excel_file_name, zip_folder, password_file):
-    """Deleting files from file handler dir. Only if files exist within the dir"""
-
-    with lock: 
-        print('* Locking file resources')
-        print('* Rinsing file handler')
-        # PDF
-        if pdf_file_name == None:
-            print('* No Pdf file found')
-        elif os.path.exists(os.path.join(UPLOAD_FOLDER, pdf_file_name)):
-            os.remove(os.path.join(UPLOAD_FOLDER, pdf_file_name))
-            print('* Pdf file found')
-            print('* Removing pdf file...')
-
-        # EXCEL
-        if excel_file_name == None:
-            print('* No Excel file found')
-        elif os.path.exists(os.path.join(UPLOAD_FOLDER, excel_file_name)):
-            os.remove(os.path.join(UPLOAD_FOLDER, excel_file_name))
-            print('* Excel file found')
-            print('* Removing Excel file...')
-
-        # NEW EXCEL
-        if new_excel_file_name == None:
-            print('* No New Excel file found')
-        elif os.path.exists(os.path.join(UPLOAD_FOLDER, new_excel_file_name)):
-            os.remove(os.path.join(UPLOAD_FOLDER, new_excel_file_name))
-            print('* New Excel file found')
-            print('* Removing New Excel file...')
-
-        # ZIP FOLDER
-        if zip_folder == None:
-            print('* No Zip folder found')
-        elif os.path.exists(os.path.join(UPLOAD_FOLDER, zip_folder)):
-            os.remove(os.path.join(UPLOAD_FOLDER, zip_folder))
-            print('* Zip folder found')
-            print('* Removing Zip folder...')
-
-        # PASSWORD FILE
-        if password_file == None:
-            print('* No Password file found')
-        elif os.path.exists(os.path.join(UPLOAD_FOLDER, password_file)):
-            os.remove(os.path.join(UPLOAD_FOLDER, password_file))
-            print('* Password file found')
-            print('* Removing Password file...')
-
-
-def run_file_check_on(file_name):
-    """Check if a file already exists"""
-    if file_name is not None:
-        os.remove(os.path.join(UPLOAD_FOLDER, file_name))
-
-
-def password_protect_excel(file_dir_path, password):
-    """Password protect file"""
-    pythoncom.CoInitialize()
-    xl_file = EnsureDispatch("Excel.Application")
-    wb = xl_file.Workbooks.Open(file_dir_path)
-    xl_file.DisplayAlerts = False
-    wb.Visible = False
-    wb.SaveAs(file_dir_path, Password=password)
-    wb.Close()
-    xl_file.Quit()
-    print("* File protected successfully")
-    pythoncom.CoUninitialize()
 
 if __name__ == '__main__':
     app.run(debug=True)
